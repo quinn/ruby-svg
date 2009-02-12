6e82cc10dc73ba614fae654195c36a412062e112
quinn <q.shanahan+service@gmail.com>
2009-02-11 23:06:49 -0600

    added fill and opacity style

--- lib/ruby-svg/processing.rb
+++ lib/ruby-svg/processing.rb
@@ -18,19 +18,45 @@ module SVG
     end
   end
 
-  #ApiMethods = :begin_shape, :vertex, :end_shape
   module Processing
     def self.included cls
       cls.class_eval do
-        attr_accessor :rp, :shapes, :polygon, :current_fill
+        attr_accessor :rp, :shapes, :polygon, :current_style
       end
     end
-  
-    # def initialize rp
-    #   @shapes = []
-    #   @rp = rp
-    # end
-  
+ 
+    def to_svg
+      svg = SVG.new width, height
+      shapes.each{|s| svg.scripts << s}
+      svg
+    end
+    
+    def save_svg_file file_counter = 0, filename = "#{title}-#{file_counter}.svg"
+      dir = "./svg_files/"
+      filepath = dir + filename
+
+      if File.exists? filepath
+        save_svg_file file_counter += 1
+      else
+        File.open(filepath,'w') << to_svg.to_s
+      end
+    end
+    
+    def reset_svg
+      @shapes = []
+    end
+    
+    def apply_styles shape
+      shape.style = current_style if current_style
+      shape
+    end
+    
+    def add_to_shapes shape
+      @shapes ||= []
+      shapes << shape
+    end
+    
+    # processing API
     def begin_shape
       @polygon = SVG::Polygon.new
       super
@@ -42,39 +68,29 @@ module SVG
     end
   
     def end_shape idk='lolk'
-      shapes << polygon
+      add_to_shapes apply_styles(polygon)
       @polygon = nil
       super
     end
 
     def ellipse x,y,w,h
-      @shapes||= []
-      shapes << SVG::Ellipse.new(x,y, w/2, h/2)
+      add_to_shapes apply_styles(SVG::Ellipse.new(x,y, w/2, h/2))
       super
     end
     
-    def to_svg
-      svg = SVG.new width, height
-      shapes.each{|s| svg.scripts << s}
-      svg
+    def fill *args
+      self.current_style = SVG::Style.new
+      
+      current_style.fill = "rgb(#{args[0]}, #{args[1]}, #{args[2]})"
+      
+      if args[3]
+        current_style.fill_opacity = (args[3]/255.0)
+      else            
+        current_style.fill_opacity = 1.0
+      end
+      
+      super
     end
-    
-    # module Frame
-    #   def self.included cls
-    #   cls.class_eval do
-    #     attr_accessor :svg
-    #     before :draw, :setup_svg
-    #     after  :draw, :log_svg
-    #   
-    #     def setup_svg
-    #       @svg = SVG.new width, height
-    #     end
-    #   
-    #     def log_svg
-    #     
-    #     end
-    #   end;end;
-    # end
   end
 end
 

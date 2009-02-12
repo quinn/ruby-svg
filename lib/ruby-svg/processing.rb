12849471f6c25f296c1c14bb5929fdc0ed0056dc
quinn <q.shanahan+service@gmail.com>
2009-02-11 21:36:20 -0600

    wip

--- lib/ruby-svg/processing.rb
+++ lib/ruby-svg/processing.rb
@@ -18,32 +18,63 @@ module SVG
     end
   end
 
-  class Processing
-    attr_accessor :rp, :shapes, :polygon
-  
-    def initialize rp
-      @shapes = []
-      @rp = rp
+  #ApiMethods = :begin_shape, :vertex, :end_shape
+  module Processing
+    def self.included cls
+      cls.class_eval do
+        attr_accessor :rp, :shapes, :polygon, :current_fill
+      end
     end
   
+    # def initialize rp
+    #   @shapes = []
+    #   @rp = rp
+    # end
+  
     def begin_shape
-      @polygon = Polygon.new
+      @polygon = SVG::Polygon.new
+      super
     end
   
     def vertex *args
       polygon.vertex *args
+      super
     end
   
     def end_shape idk='lolk'
       shapes << polygon
       @polygon = nil
+      super
+    end
+
+    def ellipse x,y,w,h
+      @shapes||= []
+      shapes << SVG::Ellipse.new(x,y, w/2, h/2)
+      super
     end
     
     def to_svg
-      svg = SVG.new rp.width, rp.height
+      svg = SVG.new width, height
       shapes.each{|s| svg.scripts << s}
       svg
     end
+    
+    # module Frame
+    #   def self.included cls
+    #   cls.class_eval do
+    #     attr_accessor :svg
+    #     before :draw, :setup_svg
+    #     after  :draw, :log_svg
+    #   
+    #     def setup_svg
+    #       @svg = SVG.new width, height
+    #     end
+    #   
+    #     def log_svg
+    #     
+    #     end
+    #   end;end;
+    # end
   end
 end
 
